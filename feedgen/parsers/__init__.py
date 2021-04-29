7f27e94329cf1e2dc909b35a7bbfa28ddd999d96
Josh Cardenzana <jvinniec@gmail.com>
2021-04-28 23:56:57 -0400

    Add CSS Parser classes to list of imports

--- feedgen/parsers/__init__.py
+++ feedgen/parsers/__init__.py
@@ -1,6 +1,7 @@
 
 
 from .parser     import Parser, ParserResult, SearchParser, TagConfig
+from .parser     import CSSInnerText, CSSAttribute
 from .googlenews import GoogleNews
 from .bingnews   import BingNews
 from .yahoonews  import YahooNews
