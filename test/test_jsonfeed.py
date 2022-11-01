f225ad9a88a85296b25d983d660837704407fee8
Josh Cardenzana <jvinniec@gmail.com>
2022-10-31 21:29:30 -0400

    (#19) Update test_jsonfeed.py to use GoogleNews for testing.

--- test/test_jsonfeed.py
+++ test/test_jsonfeed.py
@@ -7,25 +7,25 @@ from feedgen.writers import JsonFeed
 
 if __name__ == '__main__':
     # Define the parser
-    parser = YahooNews(limit=10)
+    parser = GoogleNews(limit=10)
 
     # Define the search term
     parser.search_term('cats')
 
     # Google supports a 'site' keyword that can be chained to search only
     # specific sites
-    #parser.add_site('npr.org')
+    parser.add_site('npr.org')
 
     # Run the actual parser
     results = parser.parse_html()
 
     # Save the result to an RSS file
     jsn = JsonFeed(title='test',
-               link='https://www.github.com',
-               descrip='This is only a test')
+                   link='https://www.github.com',
+                   descrip='This is only a test')
     jsn.write(results, 'test.json', pretty_print=True)
 
     # Print the titles
-    for res in results:
-        print(res.title)
-        print(f'   link: {res.link}')
+    # for res in results:
+    #     print(res.title)
+    #     print(f'   link: {res.link}')
